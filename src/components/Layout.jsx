597dd30a747308d43101ee428523b7f5855fc8c0
Leonardo de Oliveira Miranda <85960838+leonardomiranda132@users.noreply.github.com>
2025-06-26 10:01:19 -0300

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,19 +1,17 @@
-import Header from "./Header";
-import MenuLateral from "./MenuLateral";
-import { Outlet } from "react-router-dom";
-import "../styles/Layout.css";
+// Layout.jsx - Wrapper principal para organizar o layout
+import { Outlet } from 'react-router-dom';
+import MenuLateral from './MenuLateral';
+import './layout.css';
 
 export default function Layout() {
-    return (
-        <div className="layout">
-            <Header />
-            <div className="conteudo-principal">
-                <MenuLateral />
-                <main className="conteudo">
-                    <Outlet />
-                </main>
-            </div>
+  return (
+    <div className="app-layout">
+      <MenuLateral />
+      <main className="app-content">
+        <div className="content-wrapper">
+          <Outlet />
         </div>
-    );
+      </main>
+    </div>
+  );
 }
-
